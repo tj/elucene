ce108d0c0d9f5068357bbf0f3005cca1b984e3b5
TJ Holowaychuk <tj@vision-media.ca>
2014-01-30 08:10:52 -0800

    add more tests

--- test/index.js
+++ test/index.js
@@ -2,18 +2,39 @@
 var parse = require('..');
 
 describe('parse(str)', function(){
+  it('should support normal queries', function(){
+    var query = 'level:error AND user:luna';
+
+    parse(query).should.eql({
+      string: 'level:error AND user:luna'
+    });
+  })
+
   it('should support FIELDS', function(){
     var query = 'level:error FIELDS message';
+
+    parse(query).should.eql({
+      string: 'level:error',
+      fields: ['message']
+    });
   })
 
   it('should support SORT', function(){
     var query = 'level:error SORT timestamp:asc';
 
+    parse(query).should.eql({
+      string: 'level:error',
+      sort: ['timestamp:asc']
+    });
   })
 
   it('should support LIMIT', function(){
     var query = 'level:error LIMIT 2';
 
+    parse(query).should.eql({
+      string: 'level:error',
+      limit: ['2']
+    })
   })
 
   it('should support all additions', function(){
